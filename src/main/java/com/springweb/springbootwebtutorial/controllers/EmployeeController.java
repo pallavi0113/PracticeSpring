3e07283fb53a7263cc70165c1fc2974acad907de
Pallavi Kumari <paalavikr028@gmail.com>
2025-10-03 16:36:53 +0530

    new commit

--- src/main/java/com/springweb/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/springweb/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,12 +1,36 @@
 package com.springweb.springbootwebtutorial.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springweb.springbootwebtutorial.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage(){
-        return "Secret message: asdfd/@$&Asd";
+//    @GetMapping(path = "/getSecretMessage")
+//    public String getMySuperSecretMessage(){
+//        return "Secret message: asdfd/@$&Asd";
+//    }
+
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long Id){
+        return new EmployeeDTO(Id, "Pallavi","pallavi@gmail.com", 25, LocalDate.of(2025,2,1), true);
+    }
+
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
+                                  @RequestParam(required = false) String sortBy){
+        return "Hi age "+age+" "+sortBy;
+    }
+
+    @PostMapping
+    public String updateNewEmployee(){
+        return "Hello from Post";
+    }
+
+    @PutMapping
+    String updateEmployeeById(){
+        return "Hello from put";
     }
 }
