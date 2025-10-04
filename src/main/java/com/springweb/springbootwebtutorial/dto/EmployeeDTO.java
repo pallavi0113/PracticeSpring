5f0e057c6cba943dcdaa4787aaccffb91dc727e3
Pallavi Kumari <paalavikr028@gmail.com>
2025-10-04 16:48:30 +0530

    new commit

--- src/main/java/com/springweb/springbootwebtutorial/dto/EmployeeDTO.java
+++ src/main/java/com/springweb/springbootwebtutorial/dto/EmployeeDTO.java
@@ -1,9 +1,12 @@
 package com.springweb.springbootwebtutorial.dto;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.Locale;
 
 //DTO= Data Transfer Object
+@Data
 public class EmployeeDTO {
     private Long id;
 
@@ -17,64 +20,5 @@ public class EmployeeDTO {
 
     private Boolean isActive;
 
-    public EmployeeDTO(){
 
-    }
-
-    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-        this.dateOfJoining = dateOfJoining;
-        this.isActive = isActive;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public LocalDate getDateOfJoining() {
-        return dateOfJoining;
-    }
-
-    public void setDateOfJoining(LocalDate dateOfJoining) {
-        this.dateOfJoining = dateOfJoining;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
 }
